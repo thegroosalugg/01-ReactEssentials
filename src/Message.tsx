561fa2509aa4673533335ea05d8593aa79fc29c9
victor loginov <tkoe88@zoho.com>
2023-11-07 16:58:15 +0100

    app function

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,5 +1,9 @@
+// PascalCasing
 function Message() {
+  const name = 'John';
+  if (name)
+    return <h1>hello {name}</h1>;
   return <h1>hello world</h1>;
- }
+}
 
- export default Message;
+export default Message;
