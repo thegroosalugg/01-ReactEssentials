69e5bd0fad13f7324af75985070b920292ef74ab
victor loginov <tkoe88@zoho.com>
2023-11-08 22:00:19 +0100

    created list group

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,13 @@
+function ListGroup() {
+  return (
+    <ul className="list-group">
+      <li className="list-group-item">Item 1</li>
+      <li className="list-group-item">Item 2</li>
+      <li className="list-group-item">Item 3</li>
+      <li className="list-group-item">Item 4</li>
+      <li className="list-group-item">Item 5</li>
+    </ul>
+  );
+}
+
+export default ListGroup;
