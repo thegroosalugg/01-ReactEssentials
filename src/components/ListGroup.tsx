482096ae1f01f7d8fa0e1746a2fe4f1b05e2756c
victor loginov <tkoe88@zoho.com>
2023-11-09 22:59:58 +0100

    added mouse click event listenrer

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,11 +1,21 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
   const items = ["Strawberry", "Milk", "Oats", "Eggs", "Juice"];
+  const handleClick = (event: MouseEvent) => console.log(event)
   return (
     <>
       <h1>List</h1>
+      {items.length === 0 && <p>404 error</p>}
       <ul className="list-group">
         {items.map((item) => (
-          <li key={item}>{item}</li>
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={handleClick}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
