453d6ce98580dc91f1dba3f4e3a92133e07af1ac
victor loginov <tkoe88@zoho.com>
2023-11-18 14:22:38 +0100

    list groups has selected highlights

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,18 +1,25 @@
-import { MouseEvent } from "react";
+import { useState } from "react";
 
 function ListGroup() {
-  const items = ["Strawberry", "Milk", "Oats", "Eggs", "Juice"];
-  const handleClick = (event: MouseEvent) => console.log(event)
+  let items = ["Strawberry", "Milk", "Oats", "Eggs", "Juice"];
+  const [selectedIndex, setSelectedIndex] = useState(-1); // Hook
+
   return (
     <>
       <h1>List</h1>
       {items.length === 0 && <p>404 error</p>}
       <ul className="list-group">
-        {items.map((item) => (
+        {items.map((item, index) => (
           <li
-            className="list-group-item"
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
             key={item}
-            onClick={handleClick}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
           >
             {item}
           </li>
