3af99322ac6701b5b11f317c44ef7eda40dd13cb
victor loginov <tkoe88@zoho.com>
2023-11-18 16:29:08 +0100

    combined components

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,20 @@
-// import ListGroup from "./components/ListGroup";
-
-// function App() {
-//   let items = ["Strawberry", "Milk", "Oats", "Eggs", "Juice"];
-
-//   const handleSelectItem = (item: string) => {
-//     console.log(item);
-//   }
-
-//   return (
-//     <div>
-//       <ListGroup items={items} heading="Food" onSelectItem={handleSelectItem} />
-//     </div>
-//   );
-// }
-
+import ListGroup from "./components/ListGroup";
 import Alert from "./components/Alert";
 
 function App() {
+  let items = ["Strawberry", "Milk", "Oats", "Eggs", "Juice"];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  }
+
   return (
     <div>
+      <ListGroup items={items} heading="Food" onSelectItem={handleSelectItem} />
       <Alert>Everything is fine</Alert>
     </div>
   );
 }
+
 
 export default App;
