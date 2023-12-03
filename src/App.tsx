63e74122c839a971326892839c2ac1fb71941369
victor loginov <tkoe88@zoho.com>
2023-12-03 18:45:58 +0100

    moved old components to graveyard

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import ListGroup  from "./components/ListGroup";
-import Alert      from "./components/Alert";
-import Button     from "./components/Button";
-import {useState} from "react";
-
-
 function App() {
-  let items = ["Strawberry", "Milk", "Oats", "Eggs", "Juice"];
-  const [alertVisible, setAlertVisibility] = useState(false);
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
-
   return (
     <div>
-      <ListGroup items={items} heading="Food" onSelectItem={handleSelectItem} />
-      { alertVisible && <Alert onClose={() => setAlertVisibility(false)}>Pow</Alert> }
-      <Button color="success"  onClick={() => setAlertVisibility(true)}>
-        Boom
-      </Button>
+      <header>
+        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+        <h1>React Essentials</h1>
+        <p>
+          Fundamental React concepts you will need for almost any app you are
+          going to build!
+        </p>
+      </header>
+      <main>
+        <h2>Time to get started!</h2>
+      </main>
     </div>
   );
 }
