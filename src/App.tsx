341f8e526170eecdf1ac7bfb98a3a6def918f3cf
victor loginov <tkoe88@zoho.com>
2023-12-06 20:10:53 +0100

    created new react component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,35 @@
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+import ReactImg from "./assets/react-core-concepts.png"; // import images from assets folder
+import ComponentsImg from "./assets/components.png";
+
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
 function genRandomInt(max) {
+  // random number generator
   return Math.floor(Math.random() * (max + 1));
 }
 
 function Header() {
-  const description = reactDescriptions[genRandomInt(2)]
+  const description = reactDescriptions[genRandomInt(2)];
 
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={ReactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        {description} React concepts you will need for almost any app you are going to build!
+        {description} React concepts you will need for almost any app you are
+        going to build!
       </p>
     </header>
+  );
+}
+
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
   );
 }
 
@@ -23,7 +38,31 @@ function App() {
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title="Components"
+              description="First component in React"
+              image={ComponentsImg}
+            />
+            <CoreConcept
+              title="Concepts"
+              description="Second component in React"
+              image={ComponentsImg}
+            />
+            <CoreConcept
+              title="Props"
+              description="Third component in React"
+              image={ComponentsImg}
+            />
+            <CoreConcept
+              title="Love"
+              description="Fourth component in React"
+              image={ComponentsImg}
+            />
+          </ul>
+        </section>
       </main>
     </div>
   );
