86ded6eb71be5eb46ea9b9fa4a1d3d5a757b5351
victor loginov <tkoe88@zoho.com>
2023-12-12 21:14:22 +0100

    set default dynamic content value

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,33 @@
 // @ts-nocheck  // removes annoying parameter any type messages for file
 import { CORE_CONCEPTS } from "./data";
 import { EXAMPLES } from "./data";
-import { useState } from 'react';
+import { useState } from "react";
+// useState allows the dynamic rendering of content
 import Header from "./components/Header/Header";
 import CoreConcept from "./components/CoreConcept/CoreConcept";
 import TabButton from "./components/TabButton/TabButton";
 
 function App() {
-  const [dynamicContent, setContent] = useState('jsx');
+  const [dynamicContent, setContent] = useState();
+  // must always be declared at the top of the function where it is used and never nested in another function
+  // default value can be set inside parenthesis. Here no default value is set
 
   function handleSelect(clickedButton) {
-    setContent(clickedButton)
+    setContent(clickedButton);
+  }
+
+  let tabContent = <p>Select a topic</p>; // default tabContent is set prior to rendering dynamicContent
+
+  if (dynamicContent) { // if dynamicContent is set, via TabButton (onSelect paramater) then the below code is rendered
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[dynamicContent].title}</h3>
+        <p>{EXAMPLES[dynamicContent].description}</p>
+        <pre>
+          <code>{EXAMPLES[dynamicContent].code}</code>
+        </pre>
+      </div>
+    );
   }
 
   return (
@@ -37,18 +54,14 @@ function App() {
             {/* onSelect passes argument to TabButton function
             arrow function activaes when TabButton is executed (which executes onClick inside TabButton)
             arrow function executes handleSelect function above and passes along an argument, the name of selected button */}
-            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
-            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
-            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
-            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+            <TabButton onSelect={() => handleSelect("components")}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
-          <div id="tab-content">
-            <h3>{EXAMPLES[dynamicContent].title}</h3>
-            <p>{EXAMPLES[dynamicContent].description}</p>
-            <pre>
-              <code>{EXAMPLES[dynamicContent].code}</code>
-            </pre>
-          </div>
+          {tabContent}
         </section>
       </main>
     </div>
